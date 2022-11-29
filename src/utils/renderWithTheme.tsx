a1da9a233407d595d2332e6248ac2e25241117bb
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-29 20:23:02 -0300

    refactor: refatoring

--- src/utils/renderWithTheme.tsx
+++ src/utils/renderWithTheme.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { render, RenderResult } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 
-import { DefaultTheme } from "../../styles";
+import { MyTheme } from "../styles";
 
 export const renderWithTheme = (children: ReactNode): RenderResult =>
-  render(<ThemeProvider theme={DefaultTheme}>{children}</ThemeProvider>);
+  render(<ThemeProvider theme={MyTheme}>{children}</ThemeProvider>);
