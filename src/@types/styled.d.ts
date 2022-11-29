6e1d515d1bfaafb26d41ec7cca65e1d3a50ab3cc
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-29 20:22:31 -0300

    refactor: refatoring

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,10 @@
 import "styled-components";
+
 import { MyTheme } from "../styles";
 
 type ThemeType = typeof MyTheme;
 
 declare module "styled-components" {
-  export type DefaultTheme = ThemeType;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ThemeType {}
 }
