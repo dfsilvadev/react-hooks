1e5cde367fdae66b3db9705de071ceebd9406c1e
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-30 19:11:58 -0300

    feat: grid layout change

--- src/components/Panel/styles.ts
+++ src/components/Panel/styles.ts
@@ -21,21 +21,11 @@ export const PanelBody = styled.div`
   ${({ theme }) => css`
     padding-top: 0;
     display: grid;
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(auto-fit, minmax(18.875rem, 1fr));
     gap: ${theme.spacings.xsmall};
 
     header + & {
       padding-top: ${theme.spacings.xsmall};
-    }
-
-    @media (min-width: 768px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    @media (min-width: 1200px) {
-      grid-template-columns: repeat(3, 1fr);
-    }
-    @media (min-width: 1400px) {
-      grid-template-columns: repeat(4, 1fr);
     }
   `}
 `;
