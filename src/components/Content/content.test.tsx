6113951fdf5f4a969addab2b448fd175656f2b93
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-30 13:15:45 -0300

    test: add content tests

---
+++ src/components/Content/content.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/react";
+import Content from ".";
+import { renderWithTheme } from "../../utils/renderWithTheme";
+
+describe("<Content/>", () => {
+  it("should render currectly", () => {
+    const { container } = renderWithTheme(
+      <Content>
+        <h1>Content</h1>
+      </Content>
+    );
+
+    const heading = screen.getByRole("heading", { name: /content/i });
+    const parent = heading.parentElement;
+
+    expect(parent).toHaveStyle({
+      width: "100%",
+      height: "auto",
+      "min-height": "100vh",
+      padding: "2rem 0",
+
+      display: "flex",
+      "align-items": "center",
+      "justify-content": "center"
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
