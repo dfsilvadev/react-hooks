b61b4399e6a7ad7168ca6c9e5d396a523ffabbda
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-30 15:46:07 -0300

    refactor: refatoring collectCoverageFrom

--- jest.config.ts
+++ jest.config.ts
@@ -7,19 +7,17 @@ export default {
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
     "!src/components/index.ts",
-    "!src/pages/**/*.tsx",
-    "!src/pages/index.ts",
+    "!src/pages/**/*.{js,jsx,ts,tsx}",
     "!src/routes/*.{js,jsx,ts,tsx}",
     "!src/styles/**/*.ts",
     "!src/template/**/*.{js,jsx,ts,tsx}",
-    "!src/template/*.{js,jsx,ts,tsx}",
     "!src/**/stories.tsx",
     "!src/@types/**/*.d.ts",
     "!src/utils/tests/renderWithTheme.tsx",
     "!src/**/mock.ts",
     "!src/App.tsx",
     "!src/main.tsx",
-    "!vite-env.d.ts"
+    "!src/**/*.d.ts"
   ],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   modulePaths: ["<rootDir>/src", "<rootDir>/.jest"]
