63061d1292887bafe7de932a7c858fb7a472c251
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-01 15:39:36 -0300

    feat: add svg icon component

---
+++ src/components/HooksIcon/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HooksIconWrapper = styled.div`
+  width: 2rem;
+  min-width: 2rem;
+
+  svg {
+    width: 100%;
+    block-size: fit-content;
+  }
+`;
