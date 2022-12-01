7672eccb066ea72f45dba45581e58443aa450b76
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-01 15:40:50 -0300

    feat: add export default

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Card } from "./Card";
 export { default as Container } from "./Container";
 export { default as Content } from "./Content";
 export { default as Panel } from "./Panel";
+export { default as HooksIcon } from "./HooksIcon";
