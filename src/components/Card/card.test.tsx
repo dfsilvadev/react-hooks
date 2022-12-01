415c737a76538b7fb704bbccd44066aa626f5d12
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-01 18:28:05 -0300

    test: add card tests

---
+++ src/components/Card/card.test.tsx
@@ -0,0 +1,33 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "../../utils/tests/renderWithTheme";
+
+import Card from ".";
+
+const props = {
+  title: "useState",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+};
+
+describe("<Card/>", () => {
+  it("should render currectly", () => {
+    renderWithTheme(<Card {...props} />);
+
+    expect(screen.getByRole("link", { name: /useState/i })).toHaveAttribute(
+      "href",
+      "/"
+    );
+
+    expect(screen.getByRole("img")).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("heading", { name: /useState/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      )
+    ).toBeInTheDocument();
+  });
+});
