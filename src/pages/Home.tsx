91884c1eb7510467c5a056341e1a91574397872b
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-06 10:36:53 -0300

    feat: add hooks mock

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import { Card, Content, Panel } from "../components";
+
+import hooksMock from "../utils/constant";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Content>
+      <Panel title="React Hooks">
+        {hooksMock.map((item) => (
+          <Card
+            key={item.title}
+            title={item.title}
+            description={item.description}
+            url={item.url}
+          />
+        ))}
+      </Panel>
+    </Content>
+  );
 }
