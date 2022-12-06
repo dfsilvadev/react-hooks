2556ecbe1354d9391271ff8ef08a8fbe46af059c
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-06 16:22:23 -0300

    test: add paragraph tests

--- src/components/Paragraph/paragraph.test.tsx
+++ src/components/Paragraph/paragraph.test.tsx
@@ -2,14 +2,13 @@ import { screen } from "@testing-library/dom";
 import { renderWithTheme } from "../../utils/tests/renderWithTheme";
 
 import Paragraph from ".";
-
-const props = {
-  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-};
-
 describe("<Paragraph/>", () => {
   it("should render a title", () => {
-    const { container } = renderWithTheme(<Paragraph {...props} />);
+    const { container } = renderWithTheme(
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
+      </Paragraph>
+    );
 
     expect(
       screen.getByText(
