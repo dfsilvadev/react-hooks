4e0d1f9496b3ab8d3aff092a8196b032565a2fad
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-06 10:35:45 -0300

    feat: add export default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,10 @@
+export { default as Blockquote } from "./Blockquote";
 export { default as Breadcrumbs } from "./Breadcrumbs";
 export { default as Card } from "./Card";
 export { default as Container } from "./Container";
 export { default as Content } from "./Content";
+export { default as Head } from "./Head";
+export { default as Heading } from "./Heading";
 export { default as HooksIcon } from "./HooksIcon";
 export { default as Panel } from "./Panel";
 export { default as Paragraph } from "./Paragraph";
